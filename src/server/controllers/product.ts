e8cd0a8571e0d196d1b96b4387bb1ae9e2e311a3
Cristian Gomez <contact@cristiangomez.me>
2016-06-07 09:24:21 -0400

    refs #2 fixing typo.

--- src/server/controllers/product.ts
+++ src/server/controllers/product.ts
@@ -4,6 +4,6 @@ import {getProducts} from "../db";
 exports.list = function(req, res) {
   db.getProducts(function(data){
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({'products: 'data, error: false}))
+    res.send(JSON.stringify({'products: data, error: false}))
   })
 }
